2f56088180e75a9ac63d31a8f9b3627e0317de65
meihw <1290650542@qq.com>
2023-09-07 16:39:42 +0800

    111

---
+++ miniprogram/pages/components/privacy/privacy.js
@@ -0,0 +1,60 @@
+Component({
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        privacyContractName: '',
+        showPrivacy: false
+    },
+    /**
+     * 组件的生命周期
+     */
+    pageLifetimes: {
+        show() {
+          if (wx.getPrivacySetting) {
+            const _ = this
+            wx.getPrivacySetting({
+                success(res) {
+                    if (res.needAuthorization) {
+                        _.setData({
+                            privacyContractName: res.privacyContractName,
+                            showPrivacy: true
+                        })
+                    }else{
+                      _.handleAgreePrivacyAuthorization()
+                    }
+                }
+            })
+          }
+        }
+    },
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        // 打开隐私协议页面
+        openPrivacyContract() {
+            const _ = this
+            wx.openPrivacyContract({
+                fail: () => {
+                    wx.showToast({
+                        title: '遇到错误',
+                        icon: 'error'
+                    })
+                }
+            })
+        },
+        // 拒绝隐私协议
+        exitMiniProgram() {
+            // 直接退出小程序
+            wx.exitMiniProgram()
+        },
+        // 同意隐私协议
+        handleAgreePrivacyAuthorization() {
+            const _ = this
+            _.setData({
+                showPrivacy: false
+            })
+        },
+    },
+})
